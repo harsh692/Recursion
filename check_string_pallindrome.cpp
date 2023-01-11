4213460f8323a7908257f9015d2dc21efaa9e161
Harsh <72611820+harsh692@users.noreply.github.com>
2023-01-11 16:26:36 +0530

    Create check_string_pallindrome.cpp

---
+++ check_string_pallindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int checkpal(int i,string s){
+    int n=s.length();
+    if(i>=(n/2))
+    return 1;
+    else{
+        if(s[i]!=s[n-i-1])
+        return 0;
+        checkpal(i+1,s);
+    }
+}
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<checkpal(0,s);
+    return 0;
+}
